43fb8585496789018af63b802a7d4212361e0bf7
Niranga Lakshan <nirangawh@hotmail.com>
2025-08-06 16:07:51 +0530

    Added view port

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>{children}</body>
     </html>
